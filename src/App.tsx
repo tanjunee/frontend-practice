7563c59b74b38d1e750251496b8833a969f883d0
Tan, Jun Ee <junee.tan@gfk.com>
2022-05-07 19:26:22 +0800

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-const App = () => {
-  return <div>App Component</div>;
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import About from './pages/about/About';
+import Home from './pages/home/Home';
+
+const App: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
 };
 
 export default App;
